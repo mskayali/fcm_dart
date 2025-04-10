70d6356e9b923cc7db86c34e75101ed1b64b9606
Mehmet Şeref Kayalı <mskayali@yandex.com>
2025-04-10 17:38:56 +0300

    Initial commit - Spinex IO fcm Dart microservice SDK

--- lib/fcm/service.dart
+++ lib/fcm/service.dart
@@ -28,45 +28,15 @@ class FirebaseService {
   String? _projectId;
   final Map<String,que.Queue> queues ={};
 
-    final serviceCallbackUrls={
-      'spinex-turp':{
-        'success':'https://api.spinex.io/callbacks',
-        'fail':'https://api.spinex.io/callbacks',
-      },
-      'remote-patient':{
-        'success':'https://api.map2heal.com/callbacks',
-        'fail':'https://api.map2heal.com/callbacks',
-      },
-    };
+final serviceCallbackUrls={
+  '{project-ID}':{
+    'success':'{Your Success Webhook Domain}',
+    'fail':'{Your Fail Webhook Domain}',
+  },
+};
+
 final serviceAccounts = {
-      'spinex-turp':<String,dynamic>{
-        'type': 'service_account',
-        'project_id': 'spinex-turp',
-        'private_key_id': '83193cad109ba204d1ef9bdd4767df45e81a6b72',
-        'private_key':
-            '-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCWkqHafWL7+HmF\nHvk1tFW4UXgz/QI0H3tdV8CaKF2alVc66X10bMT885uJPQOQHffwj43S5Sg6uJZt\nQyE/W20TuvrhzJ8QBo8YSRgWrACQZKpS3RXF//GIinHQJFVhRPHS1thhL4uFwGJ2\nvvH0VezZzsAN0CvjGf1dydvwVRG/WtlBJAJF3fopCzz29pzi6BiUyx1IA2HjDUhI\nXHBo6zW5cwqB/pk6M74TPmZgcOGAs/HmJU+bFhi/1SjkOz9PyNNPBELotYycHxwG\nOeDEYaa8Og7Hk4Cw3GIeHUA08+OU49fxJdOCT9GYoR8VRKl7bMICq72aKyWSWSSm\n1L39myLfAgMBAAECggEADma45JkAf3as8hQGeYyldDW/6EJcfxgRva7tGhwek8Xm\ngW51Ps5BFavGZl0NEFrQF382F9kuEgEMoNJTsD0c1M8Z5sB3ktn7FAi4V+wcjl2V\nGCF9ecy4e7rjbCkazMJXhLcNcVd8TI8cJnJYhD+MTJVkiLhZMjqOoUqeEWxO1b0u\nRbRC5IZruWfZ3oIg5hd6dTibKwOiXXMqe5IBbicO/5+Sz+s4LhxqikPdx46DBEki\nj9grbP/OC/1mUMua3IXSyMMq6uBOMmQ/uIDYTMuT5q5A93h6GPqFvRCYMy3Z8C6X\nDWunFBHZdgdAzuxjDrDPnavOFhNmLvlEfmFNi2PVMQKBgQDMarFD2moeQQcnvG93\nFVQDNGlA2dsEmONgikea3La/U23g3f/aJvKcqwAGvgx1jxqVzjaQ+6ufR11clhdM\njiPlEzg6Ga/5vDujtIkshMGqGf9L7PldfLWfemYYFKFJ+0+Cb0c5gYda0ZZvs6jP\neoQs0k1Cukn8L86r93IE8s3oTwKBgQC8kZ9aKALEH5PTg5DbqsemgTIUzomeh7zL\nL066Bx4Dl9D9lHlgB+WH6xGky5+OjShGxLPD/IgMa0rCEQgPlczm1Lxpfeie/xK2\nQ9lj2oHFF7XBZtU9J0LWLbKgTCEw3G96kgHQSSEpxrhJo2/dxXnpKMvV6+MxcVIV\npVAdqtDocQKBgFEyUBdTurMpwV3XS69RmX1sZCKOZqD82dSPGMI6yZxV119qzyYo\nMlZo1inXc7t+jGDU+He+f/1Uq5ELTVXrX/auG7d4XJ4mVso/+/zi/spCaVYPWz33\nO22NgOAv0aFBYeO9r9z3xnwcpFTaWEORts7W+jl3Jcw2cTF9qJyulXuhAoGBAJzc\ncne38t0c+4am2jixbxWGQZfnW7mhZp6LAu1jAEey4aof+Xu19e1D0VCKBne52rhL\n5tJw3ued1Pls24zDa0jVzMngcELbIsC13j/fQ7YljA+PMHGHmGS8bOxFiBKyPJX0\noUeXndu3sBOpvmP4Yvpe8nlImZzGjKbu9p9HOjMxAoGAAOmrwKQrshCwPY1cO/4E\nBqjrwtkvnIsr9mkCcebM5mquPG9iUpKs3DpTyKJsh5qZ8fCRpL588sBKvQHmESGw\njqKQP355QPjC0T6JlzRwOjZhYeejTTI/85cjaM0OsJj2+lGLYdR3llYakyj9FfDG\nqFm6sEx9lPl5mjhEjCGeuz0=\n-----END PRIVATE KEY-----\n',
-        'client_email': 'firebase-adminsdk-d1yae@spinex-turp.iam.gserviceaccount.com',
-        'client_id': '107413720130120232691',
-        'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
-        'token_uri': 'https://oauth2.googleapis.com/token',
-        'auth_provider_x509_cert_url': 'https://www.googleapis.com/oauth2/v1/certs',
-        'client_x509_cert_url': 'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-d1yae%40spinex-turp.iam.gserviceaccount.com',
-        'universe_domain': 'googleapis.com',
-      },
-      'remote-patient':{
-        'type': 'service_account',
-        'project_id': 'remote-patient',
-        'private_key_id': 'abb4f05574a33221186615069593c4fb45eb65a7',
-        'private_key':
-            '-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDcCgw67dto5gvt\nJzhQvlD2H2D6N7NFCEPmdShOpMZ9qtCKhHkz6wEXIHJuMedHteuFDx3g7tAjrimH\nR+66X2Wm79eVZorZu9AI6CSwCs/iuVs52t4KrulCYRRMgAKsX7q32+8KAtNk6Tbe\n68kis0aDQ+lnWPHQisD5+dp2fyel0T+kv/MISByL0RAvFUk4pZVUgLwjrv+dikS5\nQ3/Z/4qA0AUOIRbOIy36UOYh6Im5PYPgROokpH2lSFG8pRRnE8b7TyvdAFePP/M0\nx743ZQqnkiDxhr5bUDoK0c4CwzYApyHcST0X9FqwbJo2THelmHsyqJCKPF3PGtHD\nVK85c82HAgMBAAECggEAJ8lBUN51T9BY07bFvgPLi9U8qcGsjoziCieg9CdjJJHg\nR0uQxsx1VAMv7bDkStkgDt0e3+8Of4UcSTO+fMCHFtpDNuZlsNz+zr169MHfuPUf\nA6UJhRD/aFs5WmPDF/vvPRNtc3fzVUm9/CzSH44e79k1uNQiVTzhi9GbqNVxC9Am\nOkQ32Na5VqZpzc17azL6/TgYfOOsxLfJNm1kh3kkXJtBd/duJVn+fg4u6/VM8OE5\nup4W+YA2r3Eyf8PBL+gNrEx4ERl/r+OHbItrfAVPTkteTQdMO/UH1z4qn+Zvd2SC\nrkb4bj4GC3ZmTTbo6DWKMaWpB6pDLYSWTvnANJL6kQKBgQD1sJYuXYphntsST1y/\noeFs864kDTqzQfbxl65Q7uDVJ7tdUQ+xc2LRbIPM9DRgzD20dZ31/+bKmS5PYvy0\n5ESUz9RLH8HsVfRvTRJsGeU9GpxXO3i4gGp3EkwXHM50U+yo2dx62/MtTrFzrdcL\ndC4+hXH+ZTYXpIMqNbBGf7GlZQKBgQDlReaUS7eXSygwKXJuAiBbW51IhdZ4repj\nSTC0+GQ7S1llQpnBBbeuK57utAdbTQtVUVZAlcmTxeSmbb5NMtmeLdpxfb1BwjDf\nHOu3qcY+lomV2r74bxHAHP0Y+eULABMhoN8cmMJ6YyW0FjGg7J7EiUSvUuHVJGs9\n4QesNSGeewKBgFYEkj//bfCJrod1k7JgGc2Mbz6eBxw1jyC8i0I4sCzQsU+VjtoA\n3OsXg/mg0inFuCDTQQ+cnY/3G3id3n3yoXQ8Y0Y+AyYcXe0N3dJNKq4+/9emsbhe\n9Wdk15EL+9hMfcIMLJ4zHauSLpyik3SmI36uN9/qTYrvhSClFWdJU8flAoGANKhV\nSQIK7IiqdkVOrs12OQVVnm5+z8DB8IPC2A2kl6m6onMldRunEC2clx5qAiIz1CiW\nMsc0QOhWl5Lk/j4LlTTa0u4aJZxANT4jNU+c5IsWloBjFBQOc5001COw41HlEvgr\nxhmfsRRMeIwQYfF2lZUhXVMf4yA2MUQZywd47TkCgYA8twsQr+pQ+CxKkFbbwC9S\nsarXkCWO+MH4lke/Gth+Q9OhyIl83NQIdTPSht2tfY7PmeH2gMSxZ/EqabL9Ivn7\nr/bHPE4nQ6wtSjq4jmUHvCSrtn/sSdhGWSx0Ezz0jzvY/FZhfb1IXB8cE2X15Oza\nkawTCh5Rd6oUkkFPAp0Tmg==\n-----END PRIVATE KEY-----\n',
-        'client_email': 'firebase-adminsdk-x5ga9@remote-patient.iam.gserviceaccount.com',
-        'client_id': '102258269340996148768',
-        'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
-        'token_uri': 'https://oauth2.googleapis.com/token',
-        'auth_provider_x509_cert_url': 'https://www.googleapis.com/oauth2/v1/certs',
-        'client_x509_cert_url': 'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-x5ga9%40remote-patient.iam.gserviceaccount.com',
-        'universe_domain': 'googleapis.com',
-      },
+      '{project-ID}':<String,dynamic>{FCM SECRET Json},
     };
 
   /// Loads service account credentials
